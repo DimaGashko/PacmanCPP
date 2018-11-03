29588698184f17f56a0dfefc4a0bfd9098a8065e
DimaGashko <dgashko@mail.ua>
2018-11-04 00:38:28 +0200

    initTests

---
+++ Tests/PacmanGame.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "pch.h"
+
+TEST(TestCaseNamePG, TestNamePG) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+#include "PacmanGame.h"
+
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
