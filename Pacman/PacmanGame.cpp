66c25271a8f372b26924d29034cc16f118eba99e
DimaGashko <dgashko@mail.ua>
2018-12-21 01:55:08 +0200

    madeCamera

--- Pacman/PacmanGame.cpp
+++ Pacman/PacmanGame.cpp
@@ -2,7 +2,9 @@
 
 namespace pg {
 
-	PacmanGame::PacmanGame() {
+	PacmanGame::PacmanGame() :
+		m_view(sf::Vector2f(0, 0), sf::Vector2f(0, 0))
+	{
 		_initWindow();
 	}
 
@@ -10,9 +12,9 @@ namespace pg {
 		sf::ContextSettings settings;
 		settings.antialiasingLevel = 0;
 
-		m_window.create(sf::VideoMode(1000, 600), "Figure", sf::Style::Default, settings);
+		m_window.create(sf::VideoMode(1000, 600), "Pacman", sf::Style::Default, settings);
 		m_window.setFramerateLimit(180);
-
+		
 		_updateView();
 	}
 
@@ -43,15 +45,15 @@ namespace pg {
 
 	void PacmanGame::_updateView() {
 		auto size = m_window.getSize();
-
-		m_view.setCenter(sf::Vector2f(0, 0));
 		m_view.setSize(sf::Vector2f(size.x / 2.f, size.y / 2.f));
 
 		m_window.setView(m_view);
 	}
 
 	void PacmanGame::update() {
-		//m_view.move(0.1, 0.1);
+		m_view.move(0.1, 0.1);
+		
+		_updateView();
 	}
 
 	void PacmanGame::render() {
