37929fb31b5a39a1b55ceb78ecc9f147a3d5d815
DimaGashko <dgashko@mail.ua>
2018-12-21 17:06:28 +0200

    level

---
+++ Pacman/configs/tilesets/tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="../../img/tileset.png" width="160" height="160"/>
+ <tile id="1" type="Pacman">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10" type="Wall"/>
+ <tile id="11" type="Point"/>
+</tileset>
