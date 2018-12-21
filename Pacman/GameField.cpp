6ad711e3a778819e2f0f3f0725b017bd6885e939
DimaGashko <dgashko@mail.ua>
2018-12-21 02:55:46 +0200

    initGrid

--- Pacman/GameField.cpp
+++ Pacman/GameField.cpp
@@ -2,15 +2,29 @@
 
 namespace pg {
 
-	GameField::GameField(sf::Vector2f size, sf::Vector2f cellSize, std::vector<GameObject> objects):
+	GameField::GameField(sf::Vector2f size, sf::Vector2f cellSize):
 		m_size(size),
 		m_cellSize(cellSize)
 	{
-		create(objects);
+		createGrid();
 	}
 
-	void GameField::create(std::vector<GameObject> objects) {
+	void GameField::addAllObjects(std::vector<GameObject> objects) {
+		for (auto obj : objects) {
+			addObject(obj);
+		}
+	}
+
+	void GameField::addObject(GameObject &object) {
+
+	}
+
+	void GameField::createGrid() {
+		grid.reserve(m_size.x);
 		
+		for (auto row : grid) {
+			row.reserve(m_size.y);
+		}
 	}
 
 
--- Pacman/GameField.hpp
+++ Pacman/GameField.hpp
@@ -8,15 +8,31 @@ namespace pg {
 
 	class GameField {
 	public:
-		GameField(sf::Vector2f size, sf::Vector2f cellSize, std::vector<GameObject> objects);
-		
+		GameField(sf::Vector2f size, sf::Vector2f cellSize);
+
+		/**
+		* Добавляет переданные объекты в сетку
+		* Если какой-то объект уже был в сетке, то от сначала удаляется с предыдущего места
+		* @param object добавляемый объект
+		*/
+		void addAllObjects(std::vector<GameObject> objects);
+
+		/**
+		* Добавляет объект в сетку
+		* Если объект уже был в сетке, то от сначала удаляется с предыдущего места
+		* @param object добавляемый объект
+		*/
+		void addObject(GameObject &object);
+
 		~GameField();
 	
 	private:
 		sf::Vector2f m_size;
 		sf::Vector2f m_cellSize;
+		 
+		std::vector<std::vector<std::vector<GameObject *>>> grid;
 
-		void create(std::vector<GameObject> objects);
+		void createGrid();
 		
 	};
 
