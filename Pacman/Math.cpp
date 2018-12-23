bda93a5a3f1ad1468fa3af8061d947172fd8760c
DimaGashko <dgashko@mail.ua>
2018-12-23 18:22:02 +0200

    intersectsL1

--- Pacman/Math.cpp
+++ Pacman/Math.cpp
@@ -4,4 +4,16 @@ namespace pg {
 
 	
 
+	inline bool Math::intersectsNormalRect() {
+		return false;
+	}
+
+	inline bool Math::intersectsL2() {
+		return false;
+	}
+
+	inline bool Math::intersectsL1(float a1, float a2, float b1, float b2) {
+		return (a1 < b1) ? (b1 < a2) : (a1 < b2);
+	}
+
 }; // namespace pg
--- Pacman/Math.hpp
+++ Pacman/Math.hpp
@@ -7,19 +7,15 @@ namespace pg {
 
 		// Пересекаются ли прямоугольники 
 		// (со сторонами паралельными осям)
-		static bool intersectsNormalRect() {
-
-		}
+		static bool intersectsNormalRect();
 
 		// Пересекаются ли 2-мерные отрезки
-		static bool intersectsL2() {
+		static bool intersectsL2();
 
-		}
-
-		// Пересекаются ли 1-мерные отрезки
-		static bool intersectsL1() {
-
-		}
+		// Пересекаются ли 1-мерные отрезки (a1; a2), (b1; b2)
+		// Косание не считается пересечением
+		// Параметры должны удовлетворять неравенству: a1 < a2 && b1 < b2
+		static bool intersectsL1(float a1, float a2, float b1, float b2);
 	};
 
 }; // namespace pg
