242ae49ee2b08207d2c6f4a291b5d54e79c44f10
DimaGashko <dgashko@mail.ua>
2018-12-23 19:37:36 +0200

    StaticToNamespace

--- Pacman/Math.hpp
+++ Pacman/Math.hpp
@@ -4,12 +4,10 @@
 
 namespace pg {
 
-	class Math {
-	public:
-
+	namespace Math {
 		// ѕересекаютс€ ли пр€моугольники (со сторонами паралельными ос€м)
 		// ѕереданные координаты должны быть последовательными (ax1 < ax2, by1 < by2...)
-		static bool intersectsNormalRect(
+		bool intersectsNormalRect(
 			float ax1, float ay1, float ax2, float ay2,
 			float bx1, float by1, float bx2, float by2
 		);
@@ -17,11 +15,12 @@ namespace pg {
 		// ѕересекаютс€ ли 1-мерные отрезки (a1; a2), (b1; b2)
 		//  осание не считаетс€ пересечением
 		// ѕараметры должны удовлетвор€ть неравенству: a1 < a2 && b1 < b2
-		static bool intersectsL1(float a1, float a2, float b1, float b2);
+		bool intersectsL1(float a1, float a2, float b1, float b2);
 
 		// ѕересекаютс€ ли 2-мерные отрезки
-		static bool intersectsL2(sf::Vector2f start1, sf::Vector2f end1, sf::Vector2f start2, sf::Vector2f end2);
-	};
+		bool intersectsL2(sf::Vector2f start1, sf::Vector2f end1, sf::Vector2f start2, sf::Vector2f end2);
+
+	}; // namespace Math
 
 }; // namespace pg
 
