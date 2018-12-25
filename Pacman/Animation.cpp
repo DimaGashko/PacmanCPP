40125b47b552a960f85074572e863cd0ed894783
DimaGashko <dgashko@mail.ua>
2018-12-25 16:43:00 +0200

    animationConstructor

--- Pacman/Animation.cpp
+++ Pacman/Animation.cpp
@@ -2,11 +2,18 @@
 
 namespace pg {
 
-	Animation::Animation(sf::RectangleShape *rect, sf::Texture &texture) :
-		m_rect(rect),
-		m_texture(texture)
+	Animation::Animation(
+		sf::Shape *shape,
+		sf::Texture &texture,
+		std::vector<sf::IntRect> &areas,
+		std::vector<int> &times
+	) :
+		m_shape(shape),
+		m_texture(texture),
+		m_areas(areas),
+		m_times(times)
 	{
-	
+		
 	}
 
 	Animation::~Animation() {
--- Pacman/Animation.hpp
+++ Pacman/Animation.hpp
@@ -7,14 +7,21 @@ namespace pg {
 	class Animation	{
 
 	public:
-		Animation(sf::RectangleShape *rect, sf::Texture &texture);
+		Animation(
+			sf::Shape *shape, 
+			sf::Texture &texture, 
+			std::vector<sf::IntRect> &areas, 
+			std::vector<int> &times
+		);
+		
 		~Animation();
 
 	private:
 
-		sf::RectangleShape *m_rect;
+		sf::Shape *m_shape;
 		sf::Texture m_texture;
-
+		std::vector<sf::IntRect> m_areas;
+		std::vector<int> m_times;
 	};
 
 }
