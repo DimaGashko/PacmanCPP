7c01c2e59e99262b668e15c025c6ef047022e344
DimaGashko <dgashko@mail.ua>
2019-01-08 22:46:16 +0200

    clean

--- Pacman/GObject.hpp
+++ Pacman/GObject.hpp
@@ -49,19 +49,13 @@ namespace pg {
 		float getRight();
 		float getBottom();
 
-		void setBottomLeft(sf::Vector2f coords);
-		void setTopRight(sf::Vector2f coords);
 		void setPosition(sf::Vector2f coords);
 		void setCenter(sf::Vector2f center);
-		void setEndPos(sf::Vector2f coords);
 		void setSpeed(sf::Vector2f speed);
 		void setSize(sf::Vector2f size);
 
-		sf::Vector2f getBottomLeft();
-		sf::Vector2f getTopRight();
 		sf::Vector2f getPosition();
 		sf::Vector2f getCenter();
-		sf::Vector2f getEndPos();
 		sf::Vector2f getSpeed();
 		sf::Vector2f getSize();
 
