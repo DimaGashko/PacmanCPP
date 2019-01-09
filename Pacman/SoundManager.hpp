1330cb4cc87ca3f1c940c0b07251dea5e3396e8b
DimaGashko <dgashko@mail.ua>
2019-01-09 15:29:11 +0200

    SoundManagerHeader

--- Pacman/SoundManager.hpp
+++ Pacman/SoundManager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <unordered_map>
+#include <string>
 
 #include <SFML/Audio.hpp>
 
@@ -9,11 +10,23 @@ namespace pg {
 	class SoundManager {
 	public:
 		SoundManager();
+
+		enum Type {Sound, Music};
+
+		// Возвращает SoundыSource по пути к нему
+		// Если звук ранее не добавлялся
+		// То сначала добавляется через get
+		sf::SoundSource* get(std::string url);
+
+		// Добавляет звук в SoundыManager
+		bool add(std::string url, Type type = Type::Sound);
+
 		~SoundManager();
 
 	private:
-
 		static std::unordered_map<std::string, sf::SoundSource*> s_sounds;
+
+
 	};
 
 }; // namespace pg
