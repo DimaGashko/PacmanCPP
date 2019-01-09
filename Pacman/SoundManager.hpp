dd9960d56a9a6f5954f6db2ecc8580cd246d68f3
DimaGashko <dgashko@mail.ua>
2019-01-09 15:06:59 +0200

    soundManagerEdit

--- Pacman/SoundManager.hpp
+++ Pacman/SoundManager.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
+#include <unordered_map>
+
+#include <SFML/Audio.hpp>
+
 namespace pg {
 
 	class SoundManager {
 	public:
 		SoundManager();
 		~SoundManager();
+
+	private:
+
+		static std::unordered_map<std::string, sf::SoundSource*> s_sounds;
 	};
 
 }; // namespace pg
