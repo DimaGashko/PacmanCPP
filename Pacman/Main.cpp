28180b075b0a4e9727069e60ba085bb73254b75f
DimaGashko <dgashko@mail.ua>
2019-01-11 11:34:48 +0200

    clean

--- Pacman/Main.cpp
+++ Pacman/Main.cpp
@@ -2,25 +2,11 @@
 #include <string>
 
 #include "PacmanGame.hpp"
-#include "Wall.hpp"
-#include "Singleton.hpp"
 
 using namespace std;
 
 int main() {
 	srand((int)time(0));
-
-	pg::Wall *wall = pg::Singleton<pg::Wall>::getInstance();
-
-	pg::Wall *wall2 = pg::Singleton<pg::Wall>::getInstance();
-
-	wall2->setLeft(111);
-	cout << wall->getLeft() << endl;
-
-	wall->setTop(222);
-	cout << wall2->getTop() << endl;
-
-	system("pause");
 
 	//system("pause");
 
