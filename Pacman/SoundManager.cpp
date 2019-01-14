5554764fca882415fbdc41e2ae1514dab2b93c75
DimaGashko <dgashko@mail.ua>
2019-01-14 10:35:40 +0200

    soundManager

--- Pacman/SoundManager.cpp
+++ Pacman/SoundManager.cpp
@@ -31,10 +31,21 @@ namespace pg {
 	bool SoundManager::add(std::string url) {
 		if (m_sounds[url] != nullptr) return true;
 
+		auto buffer = m_buffers->get(url);
+		if (!buffer) return false;
 
+		auto *sound = new sf::Sound(*buffer);
+
+		m_sounds[url] = sound;
+
+		return true;
 	}
 
 	sf::Sound* SoundManager::get(std::string url) {
+		if (m_sounds[url] == nullptr) {
+			add(url);
+		}
+
 		return m_sounds[url];
 	}
 
