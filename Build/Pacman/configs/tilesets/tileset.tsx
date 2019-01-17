d26573fd1fa9d6462497f417b1c3fb1581b2cb24
DimaGashko <dgashko@mail.ua>
2019-01-17 21:58:42 +0200

    build

---
+++ Build/Pacman/configs/tilesets/tileset.tsx
@@ -0,0 +1,340 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tileset" tilewidth="16" tileheight="16" tilecount="200" columns="20">
+ <image source="../../img/tileset.png" width="320" height="160"/>
+ <tile id="0" type="Pacman">
+  <properties>
+   <property name="animationName" value="right"/>
+   <property name="animation_bottom" type="int" value="60"/>
+   <property name="animation_die" type="int" value="3"/>
+   <property name="animation_left" type="int" value="20"/>
+   <property name="animation_stayBottom" type="int" value="61"/>
+   <property name="animation_stayLeft" type="int" value="21"/>
+   <property name="animation_stayRight" type="int" value="1"/>
+   <property name="animation_stayTop" type="int" value="41"/>
+   <property name="animation_top" type="int" value="40"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="5" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+   <frame tileid="8" duration="150"/>
+   <frame tileid="9" duration="150"/>
+   <frame tileid="10" duration="150"/>
+   <frame tileid="11" duration="150"/>
+   <frame tileid="12" duration="150"/>
+   <frame tileid="13" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="2" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="22" type="Blinky">
+  <properties>
+   <property name="animationName" value="right"/>
+   <property name="animation_afraid" type="int" value="80"/>
+   <property name="animation_bottom" type="int" value="28"/>
+   <property name="animation_left" type="int" value="24"/>
+   <property name="animation_stayBottom" type="int" value="29"/>
+   <property name="animation_stayLeft" type="int" value="25"/>
+   <property name="animation_stayRight" type="int" value="23"/>
+   <property name="animation_stayTop" type="int" value="27"/>
+   <property name="animation_top" type="int" value="26"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="22" duration="150"/>
+   <frame tileid="23" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="150"/>
+   <frame tileid="25" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="25" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="26">
+  <animation>
+   <frame tileid="26" duration="150"/>
+   <frame tileid="27" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="27">
+  <animation>
+   <frame tileid="27" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <animation>
+   <frame tileid="28" duration="150"/>
+   <frame tileid="29" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <animation>
+   <frame tileid="29" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="250"/>
+   <frame tileid="41" duration="250"/>
+   <frame tileid="2" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="41" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="42" type="Pinky">
+  <properties>
+   <property name="animationName" value="right"/>
+   <property name="animation_afraid" type="int" value="80"/>
+   <property name="animation_bottom" type="int" value="48"/>
+   <property name="animation_left" type="int" value="44"/>
+   <property name="animation_stayBottom" type="int" value="49"/>
+   <property name="animation_stayLeft" type="int" value="45"/>
+   <property name="animation_stayRight" type="int" value="43"/>
+   <property name="animation_stayTop" type="int" value="47"/>
+   <property name="animation_top" type="int" value="46"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="42" duration="150"/>
+   <frame tileid="43" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="43">
+  <animation>
+   <frame tileid="42" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="44">
+  <animation>
+   <frame tileid="44" duration="150"/>
+   <frame tileid="45" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="45">
+  <animation>
+   <frame tileid="44" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="46">
+  <animation>
+   <frame tileid="46" duration="150"/>
+   <frame tileid="47" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="47" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <animation>
+   <frame tileid="48" duration="150"/>
+   <frame tileid="49" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="49">
+  <animation>
+   <frame tileid="49" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="60" duration="250"/>
+   <frame tileid="61" duration="250"/>
+   <frame tileid="2" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="61">
+  <animation>
+   <frame tileid="61" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="62" type="Inky">
+  <properties>
+   <property name="animationName" value="right"/>
+   <property name="animation_afraid" type="int" value="80"/>
+   <property name="animation_bottom" type="int" value="68"/>
+   <property name="animation_left" type="int" value="64"/>
+   <property name="animation_stayBottom" type="int" value="69"/>
+   <property name="animation_stayLeft" type="int" value="65"/>
+   <property name="animation_stayRight" type="int" value="63"/>
+   <property name="animation_stayTop" type="int" value="67"/>
+   <property name="animation_top" type="int" value="66"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="62" duration="150"/>
+   <frame tileid="63" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="63">
+  <animation>
+   <frame tileid="63" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="64">
+  <animation>
+   <frame tileid="64" duration="150"/>
+   <frame tileid="65" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="65">
+  <animation>
+   <frame tileid="65" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="66">
+  <animation>
+   <frame tileid="66" duration="150"/>
+   <frame tileid="67" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="67">
+  <animation>
+   <frame tileid="67" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <animation>
+   <frame tileid="68" duration="150"/>
+   <frame tileid="69" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="69">
+  <animation>
+   <frame tileid="69" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="80">
+  <animation>
+   <frame tileid="80" duration="250"/>
+   <frame tileid="81" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="81">
+  <animation>
+   <frame tileid="81" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="82" type="Clyde">
+  <properties>
+   <property name="animationName" value="right"/>
+   <property name="animation_afraid" type="int" value="80"/>
+   <property name="animation_bottom" type="int" value="88"/>
+   <property name="animation_left" type="int" value="84"/>
+   <property name="animation_stayBottom" type="int" value="89"/>
+   <property name="animation_stayLeft" type="int" value="85"/>
+   <property name="animation_stayRight" type="int" value="83"/>
+   <property name="animation_stayTop" type="int" value="87"/>
+   <property name="animation_top" type="int" value="86"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="82" duration="150"/>
+   <frame tileid="83" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="83">
+  <animation>
+   <frame tileid="83" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="84" duration="150"/>
+   <frame tileid="85" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="85">
+  <animation>
+   <frame tileid="85" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="86">
+  <animation>
+   <frame tileid="86" duration="150"/>
+   <frame tileid="87" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="87">
+  <animation>
+   <frame tileid="87" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="88">
+  <animation>
+   <frame tileid="88" duration="150"/>
+   <frame tileid="89" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="89">
+  <animation>
+   <frame tileid="89" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="101" type="Bonus"/>
+ <tile id="102" type="Bonus"/>
+ <tile id="103" type="Bonus"/>
+ <tile id="104" type="Bonus"/>
+ <tile id="105" type="Bonus"/>
+ <tile id="106" type="Bonus"/>
+ <tile id="107" type="Key"/>
+ <tile id="108" type="BigPoint"/>
+ <tile id="109" type="Point">
+  <objectgroup draworder="index">
+   <object id="1" x="6" y="7" width="4" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="140" type="Wall"/>
+ <tile id="150" type="Bonus"/>
+</tileset>
